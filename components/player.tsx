2e448c28311817e71d718c0e9681d888b80578e2
mohsen-rh214 <mohsen.rh0214@gmail.com>
2023-12-20 12:13:06 +0330

    player states init

--- components/player.tsx
+++ components/player.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Flex } from "@chakra-ui/layout";
 import { ButtonGroup, IconButton, RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import ReactHowler from 'react-howler';
 import {
     MdShuffle,
@@ -10,34 +11,65 @@ import {
     MdOutlineRepeat,
 } from 'react-icons/md'
 
-const Player = () => {
+const Player = ({ songs, activeSong }) => {
+    const [playing, setPlaying] = useState(true)
+    const [indes, setIndes] = useState(0)
+    const [seek, setSeek] = useState(0.0)
+    const [repeat, setRepeat] = useState(false)
+    const [shuffle, setShuffle] = useState(false)
+    const [duration, setDuration] = useState(0.0)
+
+    const setPlayState = (value) => {
+        setPlaying(value)
+    }
+
+    const onShuffle = () => {
+        setShuffle((state)=>!state)
+    }
+    const onRepeat = () => {
+        setRepeat((state)=>!state)
+    }
+
     return (
         <Box>
-            <Box></Box>
+            <Box>
+                {/* <ReactHowler
+                    playing={playing}
+                    src={activeSong?.url}
+
+                /> */}
+            </Box>
             <Center>
                 <ButtonGroup>
                     <IconButton outline='none'
                         variant='link' aria-label="shuffle"
                         fontSize='24px'
                         icon={<MdShuffle />}
+                        color={shuffle ? 'white' : 'gray.600'}
+                        onClick={onShuffle}
                     />
                     <IconButton outline='none'
                         variant='link' aria-label="skip"
                         fontSize='24px'
                         icon={<MdSkipPrevious />}
                     />
-                    <IconButton outline='none'
-                        variant='link' aria-label="play"
-                        fontSize='40px'
-                        color='white'
-                        icon={<MdOutlinePlayCircleFilled />}
-                    />
-                    <IconButton outline='none'
-                        variant='link' aria-label="pause"
-                        fontSize='40px'
-                        color='white'
-                        icon={<MdOutlinePauseCircleFilled />}
-                    />
+                    {playing ? (
+                        <IconButton outline='none'
+                            variant='link' aria-label="pause"
+                            fontSize='40px'
+                            color='white'
+                            icon={<MdOutlinePauseCircleFilled />}
+                            onClick={() => setPlayState(false)}
+                        />
+                    ) : (
+                        <IconButton outline='none'
+                            variant='link' aria-label="play"
+                            fontSize='40px'
+                            color='white'
+                            icon={<MdOutlinePlayCircleFilled />}
+                            onClick={() => setPlayState(true)}
+                        />
+                    )}
                     <IconButton outline='none'
                         variant='link' aria-label="next"
                         fontSize='24px'
@@ -47,6 +79,8 @@ const Player = () => {
                         variant='link' aria-label="repeat"
                         fontSize='24px'
                         icon={<MdOutlineRepeat />}
+                        color={repeat ? 'white' : 'gray.600'}
+                        onClick={onRepeat}
                     />
                 </ButtonGroup>
             </Center>
@@ -68,7 +102,7 @@ const Player = () => {
                                 bg='gray.800'
                             >
                                 <RangeSliderFilledTrack bg='gray.600' />
-                                <RangeSliderThumb index={0}/>
+                                <RangeSliderThumb index={0} />
                             </RangeSliderTrack>
                         </RangeSlider>
                     </Box>
