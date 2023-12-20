273decabfb858ca0d909ad48e1b05f92887554a7
mohsen-rh214 <mohsen.rh0214@gmail.com>
2023-12-20 17:46:51 +0330

    player functionality

--- components/player.tsx
+++ components/player.tsx
@@ -1,6 +1,7 @@
 import { Box, Center, Flex } from "@chakra-ui/layout";
 import { ButtonGroup, IconButton, RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { useStoreActions } from "easy-peasy";
+import { useEffect, useRef, useState } from "react";
 import ReactHowler from 'react-howler';
 import {
     MdShuffle,
@@ -10,34 +11,111 @@ import {
     MdOutlinePauseCircleFilled,
     MdOutlineRepeat,
 } from 'react-icons/md'
+import { formatTime } from "../lib/formatters";
+
 
 const Player = ({ songs, activeSong }) => {
     const [playing, setPlaying] = useState(true)
-    const [indes, setIndes] = useState(0)
+    const [index, setIndex] = useState(
+        songs.findIndex((s) => s.id === activeSong.id)
+    )
     const [seek, setSeek] = useState(0.0)
     const [repeat, setRepeat] = useState(false)
     const [shuffle, setShuffle] = useState(false)
     const [duration, setDuration] = useState(0.0)
+    const [isSeeking, setIsSeeking] = useState(false)
+    const soundRef = useRef(null)
+    const repeatRef = useRef(repeat)
+    const setActiveSong = useStoreActions((state: any) => state.changeActiveSong)
+
+    useEffect(() => {
+        let timerId
+
+        if (playing && !isSeeking) {
+            const f = () => {
+                setSeek(soundRef.current.seek())
+                timerId = requestAnimationFrame(f)
+            }
+
+            timerId = requestAnimationFrame(f)
+            return () => cancelAnimationFrame(timerId)
+        }
+
+        cancelAnimationFrame(timerId)
+    }, [playing, isSeeking])
+
+    useEffect(() => {
+        setActiveSong(songs[index])
+    }, [index, setActiveSong, songs])
+
+    useEffect(() => {
+        repeatRef.current = repeat;
+    }, [repeat])
+
 
     const setPlayState = (value) => {
         setPlaying(value)
     }
 
     const onShuffle = () => {
-        setShuffle((state)=>!state)
+        setShuffle((state) => !state)
     }
     const onRepeat = () => {
-        setRepeat((state)=>!state)
+        setRepeat((state) => !state)
+    }
+
+    const prevSong = () => {
+        setIndex((state) => {
+            return state ? state - 1 : songs.length - 1;
+        })
+    }
+    const nextSong = () => {
+        setIndex((state: any) => {
+            if (shuffle) {
+                // shuffle logic
+                const next = Math.floor(Math.random() * songs.length)
+
+                if (next === state) {
+                    return nextSong();
+                }
+
+                return next;
+
+            } else {
+                return state === songs.length - 1 ? 0 : state + 1;
+            }
+        })
+    }
+
+    const onEnd = () => {
+        if (repeatRef.current) {
+            setSeek(0)
+            soundRef.current.seek(0)
+        } else {
+            nextSong()
+        }
+    }
+
+    const onLoad = () => {
+        const songDuration = soundRef.current.duration();
+        setDuration(songDuration)
+    }
+
+    const onSeek = (e) => {
+        setSeek(parseFloat(e[0])) // update visual ui thumb
+        soundRef.current.seek(e[0]) // song play position
     }
 
     return (
         <Box>
             <Box>
-                {/* <ReactHowler
+                <ReactHowler
+                    ref={soundRef}
                     playing={playing}
                     src={activeSong?.url}
-
-                /> */}
+                    onLoad={onLoad}
+                    onEnd={onEnd}
+                />
             </Box>
             <Center>
                 <ButtonGroup>
@@ -52,6 +130,7 @@ const Player = ({ songs, activeSong }) => {
                         variant='link' aria-label="skip"
                         fontSize='24px'
                         icon={<MdSkipPrevious />}
+                        onClick={prevSong}
                     />
                     {playing ? (
                         <IconButton outline='none'
@@ -74,6 +153,7 @@ const Player = ({ songs, activeSong }) => {
                         variant='link' aria-label="next"
                         fontSize='24px'
                         icon={<MdSkipNext />}
+                        onClick={nextSong}
                     />
                     <IconButton outline='none'
                         variant='link' aria-label="repeat"
@@ -88,26 +168,28 @@ const Player = ({ songs, activeSong }) => {
             <Box color='gray.600'>
                 <Flex justify='center' align='center'>
                     <Box width='10%'>
-                        <Text fontSize='xs'>1:26</Text>
+                        <Text fontSize='xs'>{formatTime(seek)}</Text>
                     </Box>
                     <Box width='80%'>
                         <RangeSlider
                             aria-label={['min', 'max']}
                             step={0.1}
                             min={0}
-                            max={300}
                             id="player-range"
+                            max={duration ? duration.toFixed(2) : 0}
+                            onChange={onSeek}
+                            value={[seek]}
+                            onChangeStart={() => setIsSeeking(true)}
+                            onChangeEnd={() => setIsSeeking(true)}
                         >
-                            <RangeSliderTrack
-                                bg='gray.800'
-                            >
+                            <RangeSliderTrack bg='gray.800'>
                                 <RangeSliderFilledTrack bg='gray.600' />
-                                <RangeSliderThumb index={0} />
                             </RangeSliderTrack>
+                            <RangeSliderThumb index={0} />
                         </RangeSlider>
                     </Box>
                     <Box width='10%' textAlign='right'>
-                        <Text fontSize='xs'>3:23</Text>
+                        <Text fontSize='xs'>{formatTime(duration)}</Text>
                     </Box>
                 </Flex>
             </Box>
